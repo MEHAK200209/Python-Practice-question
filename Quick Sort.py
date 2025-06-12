b457f4b1066810202d875d4a4bcd3689321974de
Mehak <111582264+MEHAK200209@users.noreply.github.com>
2025-06-12 15:23:13 +0530

    Add files via upload

---
+++ Quick Sort.py
@@ -0,0 +1,12 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        left = [x for x in arr[1:] if x < pivot]
+        right = [x for x in arr[1:] if x >= pivot]
+        return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+arr2 = [10, 7, 8, 9, 1, 5]
+print("Quick Sort:", quick_sort(arr2))
